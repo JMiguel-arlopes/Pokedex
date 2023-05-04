ffe5bd73aad897c549839b31866ed5a0888b41c6
JM-arlopes <jmiguelarlopes@gmail.com>
2023-05-04 00:05:16 -0300

    automatizando tipagens pokemon

--- js/main.js
+++ js/main.js
@@ -1,12 +1,39 @@
 const pokemonName = document.querySelector('.name__data');
 const pokemonID = document.querySelector('.number__data');
 const pokemonImage = document.querySelector('.img__pokemon');
+
 const form = document.querySelector('.formulario');
 const input = document.querySelector('.input__search');
+
 const btnNext = document.querySelector('.btn-next');
 const btnPrev = document.querySelector('.btn-prev');
+const btnSubmit = document.querySelector('.btn-submit');
+
+const tipagens = document.querySelectorAll('.tipo');
 
 let pokemonAtual = 1;
+
+const listaTipos = [
+    "fire",
+    "water",
+    "grass",
+    'poison',
+    'flying',
+    'bug',
+    'normal',
+    'electric',
+    'rock',
+    'ground',
+    'fairy',
+    'fighting',
+    'steel',
+    'ice',
+    'ghost',
+    'psychic',
+    'dragon',
+    'dark'
+]
+
 
 const buscaPokemon = async (pokemon) => {
     const APIResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
@@ -17,6 +44,7 @@ const buscaPokemon = async (pokemon) => {
     }
 }
 
+
 const renderizaPokemon = async (pokemon) => {
 
     pokemonName.innerHTML = 'Carregando...';
@@ -24,41 +52,72 @@ const renderizaPokemon = async (pokemon) => {
 
     const data = await buscaPokemon(pokemon);
 
-    if(data) {
+    if (data) {
+
         pokemonImage.style.display = 'block';
         pokemonName.innerHTML = data.name;
         pokemonID.innerHTML = data.id;
-
+    
         if (data.id >= 650 && data.id <= 721) {
-            pokemonImage.src = data['sprites']['versions']['generation-vi']['omegaruby-alphasapphire']['front_default'];
+            pokemonImage.src = data['sprites']['versions']['generation-vi']['omegaruby-alphasapphire']['front_default']; 
         } else {
             if (data.id >= 722) {
                 pokemonImage.src = data['sprites']['versions']['generation-viii']['icons']['front_default'];
             } else {
-                    pokemonImage.src = data['sprites']['versions']['generation-v']['black-white']['animated']['front_default'];
-                }
+                pokemonImage.src = data['sprites']['versions']['generation-v']['black-white']['animated']['front_default'];
             }
+        }
+
         pokemonAtual = data.id;
+
+        tipagens.forEach((boxType, i) => {
+
+            let sourceType = data['types'][i]['type']['name'];
+
+            const [, spanTipoSecundario] = tipagens;
+            const verificaClassSecundaria = boxType.classList[1];
+
+            boxType.innerHTML = '';
+            boxType.innerHTML = sourceType;
+
+            if(i === 0) {
+                spanTipoSecundario.classList.add('hide');
+            } else {
+                spanTipoSecundario.classList.remove('hide');
+            }
+
+            listaTipos.forEach(tipagem => {
+                if (sourceType === tipagem) {
+                    boxType.classList.remove(verificaClassSecundaria);
+                    boxType.classList.add(tipagem);
+                }
+            })
+        })
+        
     } else {
         pokemonName.innerHTML = 'not found :(';
         pokemonID.innerHTML = '';
         pokemonImage.style.display = 'none';
-    }
 
-    input.value = '';
+        tipagens.forEach((boxType) => {
+            boxType.innerHTML = '';
+            const verificaClassSecundaria = boxType.classList[1];
+            boxType.classList.remove(verificaClassSecundaria);
+        })
+    }
 }
 
 form.addEventListener('submit', (event) => {
     event.preventDefault();
     renderizaPokemon(input.value.toLowerCase());
+    input.value = '';
 })
 
-btnPrev.addEventListener('click', () => {
-    if( pokemonAtual > 1) {
+btnPrev.addEventListener("click", () => {
+    if(pokemonAtual > 1) {
         pokemonAtual -= 1;
         renderizaPokemon(pokemonAtual);
-    }
-    
+    } 
 })
 
 btnNext.addEventListener('click', () => {
